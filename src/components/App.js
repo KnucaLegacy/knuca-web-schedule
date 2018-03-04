f9ec5db442ac5bd47cd81fd0d3922454cc0cf7c2
Mark Shulhin <mark.shulhin@gmail.com>
2018-03-04 18:28:22 +0200

    Added initial markup and styles. Added Bootstrap and reactstrap to the project

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import background from "../img/background.jpg";
+import { Button, Input, InputGroupAddon, InputGroup, Row, Col } from "reactstrap";
+import "../App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <img src={background} alt="background" className="bg" />
+            <InputGroup className="main-input col-8">
+              <Input placeholder="Введите группу\преподаватель" />
+              <InputGroupAddon addonType="prepend">
+                <Button color="success">Search</Button>
+              </InputGroupAddon>
+            </InputGroup>
+
+      </div>
+    );
+  }
+}
+
+export default App;
