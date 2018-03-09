d0699e243b4e0715577d41c6a876403190424070
MarkShulhin <mark.shulhin@gmail.com>
2018-03-09 19:55:24 +0200

    Changed favicon. Background image for now is attached to the body instead of img. Added box shadow to input. Travis CI now uses yarn only

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
-import background from "../img/background.jpg";
-import { Button, Input, InputGroupAddon, InputGroup } from "reactstrap";
+import { Container, Button, Input, InputGroupAddon, InputGroup } from "reactstrap";
 import "../App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <img src={background} alt="background" className="bg" />
-            <InputGroup className="main-input col-8">
+      <Container>
+            <InputGroup className="main-input col-8 ">
               <Input placeholder="Введите группу\преподаватель" />
               <InputGroupAddon addonType="prepend">
                 <Button color="success">Search</Button>
               </InputGroupAddon>
             </InputGroup>
-      </div>
+      </Container>
     );
   }
 }
