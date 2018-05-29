b5dcfd8592311e483ad9fb55a13f8b900087ec5f
Mark Shulhin <mark.shulhin@gmail.com>
2018-05-29 23:41:04 +0300

    BYE BYE PROXY!! 🙅‍♂️

--- src/api/LessonsDataManager.js
+++ src/api/LessonsDataManager.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 import config from '../config/config';
 
 export async function getLessons(endPoint) {
-  const { serverApiUrl, port, proxy } = config;
-  const url = `${proxy}/${serverApiUrl}:${port}/${endPoint}`;
+  const { serverApiUrl, port } = config;
+  const url = `${serverApiUrl}:${port}/${endPoint}`;
   
   const lessons = await axios(url);
     
--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,5 @@
 export default {
   serverApiUrl: 'http://35.195.191.179',
   port: '8080',
-  proxy: 'https://cors-anywhere.herokuapp.com',
 };
 
