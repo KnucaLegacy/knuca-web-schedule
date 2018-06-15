e3e6f1c11f78cddba3ba020c38bdbbf62ed40a41
Mark Shulhin <mark.shulhin@gmail.com>
2018-06-15 15:52:40 +0300

    BE have new home

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export default {
-  serverApiUrl: 'https://cors-anywhere.herokuapp.com/http://35.195.191.179',
+  serverApiUrl: 'https://cors-anywhere.herokuapp.com/http://35.206.151.97',
   port: '8080',
 };
 
