85fbf3a56f6776d5a6c7a3a3aec375069ef44fd0
Mark Shulhin <mark.shulhin@gmail.com>
2018-08-26 16:05:24 +0300

    News mid section layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Button } from 'reactstrap';
 
 import LessonsContainer from '../containers/LessonsContainer';
 import SearchContainer from '../containers/SearchContainer';
 
 import benefitsImage from '../img/benefits.png';
 import monitorImage from '../img/search-monitor.png';
+import tgPhonesImage from '../img/tg-phones.png';
 
 import '../css/App.css';
 import '../css/Grid.css';
@@ -24,17 +25,35 @@ const App = () => (
     </section>
     <section className="news-container">
       <Container>
-        <Row className="news-item">
-          <Col className="up-left" lg={6}>
+        <Row className="news-item upper">
+          <Col lg={6} className="upper-left">
             <img src={benefitsImage} alt="benefits" />
           </Col>
-          <Col lg={{ size: 5, offset: 1 }} className="up-right">
+          <Col lg={{ size: 5, offset: 1 }} className="upper-right">
             <img src={monitorImage} alt="search monitor" />
           </Col>
         </Row>
-        <Row>
-          <Col />
-          <Col />
+        <Row className="news-item mid">
+          <Col lg={7} className="mid-left">
+            <img src={tgPhonesImage} alt="telegram bot on phones" />
+          </Col>
+          <Col lg={5} className="mid-right">
+            <div className="text-block">
+              <h2>Легко, швидко і точно</h2>
+              <p>Телеграм бот відповість</p>
+              <p>на всі ваші запити</p>
+              <span>@Knuca_ScheduleBot</span>
+              <Button
+                  size="md"
+                  color="info"
+                  tag="a"
+                  href="http://knuca.com"
+                  className="text-block-link"
+              >
+              Приєднуйся
+              </Button>
+            </div>
+          </Col>
         </Row>
       </Container>
     </section>
