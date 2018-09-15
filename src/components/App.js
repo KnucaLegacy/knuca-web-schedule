5bda7cb02fee49f060be38cbfe9bb35fecf796b9
Mark Shulhin <mark.shulhin@gmail.com>
2018-09-15 13:59:31 +0300

    Better code split

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
 import LessonsContainer from '../containers/LessonsContainer';
 import SearchContainer from '../containers/SearchContainer';
-import Image from './Image';
-
-import images from '../img';
-
+import News from './News';
+import Footer from './Footer';
 
 import '../css/App.css';
 import '../css/Grid.css';
@@ -26,111 +24,8 @@ const App = () => (
         </Row>
       </Container>
     </section>
-    <section className="news-container">
-      <Container>
-        <Row className="news-item upper">
-          <Col xl={6} lg={7} md={12} sm={12} className="upper-left">
-            <Image
-                xl={images.xl.benefitsImage}
-                lg={images.lg.benefitsImageLg}
-                md={images.md.benefitsImageMd}
-                sm={images.sm.benefitsImageSm}
-                alt="Benefits"
-            />
-          </Col>
-          <Col xl={{ size: 5, offset: 1 }} lg={5} md={12} sm={12} className="upper-right">
-            <Image
-                xl={images.xl.monitorImage}
-                lg={images.lg.monitorImageLg}
-                sm={images.sm.monitorImageSm}
-                alt="Search monitor"
-            />
-          </Col>
-        </Row>
-        <Row className="news-item mid">
-          <Col xl={7} lg={7} md={12} sm={12} className="mid-left">
-            <Image
-                xl={images.xl.tgPhonesImage}
-                lg={images.lg.tgPhonesImageLg}
-                sm={images.sm.tgPhonesImageSm}
-                alt="Telegram bot on phones"
-            />
-          </Col>
-          <Col xl={5} lg={5} md={12} sm={12} className="mid-right">
-            <div className="text-block">
-              <h2>Легко, швидко і точно</h2>
-              <p>Телеграм бот відповість</p>
-              <p>на всі ваші запити</p>
-              <span>@Knuca_ScheduleBot</span>
-              <Button
-                  size="md"
-                  color="info"
-                  tag="a"
-                  href="https://t.me/KNUCA_ScheduleBot"
-                  className="text-block-link"
-              >
-              Приєднуйся
-              </Button>
-            </div>
-          </Col>
-        </Row>
-        <Row className="news-item low">
-          <Col xl={6} lg={7} md={12} sm={12} className="low-left">
-            <div className="text-block">
-              <h2>Особливості та переваги</h2>
-              <p>Бот дозволяє здійснювати пошук</p>
-              <p>за групою або викладачем</p>
-              <p>Також ви можете вказати дату</p>
-              <p>або діапазон дат</p>
-            </div>
-          </Col>
-          <Col xl={6} lg={{ size: 5, offset: 0 }} md={12} sm={12}className="low-right">
-            <Image
-                xl={images.xl.tgScreenshots}
-                lg={images.lg.tgScreenshotsLg}
-                sm={images.sm.tgScreenshotsSm}
-                alt="Telegram screenshots"
-            />
-          </Col>
-        </Row>
-      </Container>
-    </section>
-    <footer>
-      <Container>
-        <Row>
-          <Col className="page-footer">
-            <div className="footer-credits">
-              <Image
-                  xl={images.xl.KnucaLegacyLogo}
-                  lg={images.lg.KnucaLegacyLogoLg}
-                  sm={images.sm.KnucaLegacyLogoSm}
-                  alt="Knuca Legacy logo"
-              />
-              <span>&copy; Knuca Legacy 2018, All rights reserved.</span>
-            </div>
-            <div className="footer-contacts">
-              <span>Зв`яжіться з нами</span>
-              <a href="knuca-legacy.gmail.com">
-                <Image
-                    xl={images.xl.gmailLogo}
-                    lg={images.lg.gmailLogoLg}
-                    sm={images.sm.gmailLogoSm}
-                    alt="Gmail logo"
-                />
-              </a>
-              <a href="https://t.me/KNUCA_ScheduleBot">
-                <Image
-                    xl={images.xl.tgLogo}
-                    lg={images.lg.tgLogoLg}
-                    sm={images.sm.tgLogoSm}
-                    alt="Knuca Legacy logo"
-                />
-              </a>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </footer>
+    <News />
+    <Footer />
   </main>
 );
 
