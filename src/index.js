9d853c3c47f521fde444155187186c6b695c422e
Mark Shulhin <mark.shulhin@gmail.com>
2019-07-02 15:19:19 +0300

    Removed service worker to see if it breaks the requests

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import ReactGA from 'react-ga';
 import 'bootstrap/dist/css/bootstrap.css';
 import './css/open-iconic-bootstrap.min.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactGA.initialize('UA-112398090-2');
 ReactGA.pageview(window.location.pathname);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
